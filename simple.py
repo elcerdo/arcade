64d91e9f3435d593b6b72b6784d69eb02c3f696f
Pierre Gueth <pierre.gueth@creatis.insa-lyon.fr>
2012-11-29 15:25:40 +0100

    dumb

--- simple.py
+++ simple.py
@@ -10,7 +10,7 @@ pygame.init()
 resolution = (800,600)
 background_color = (0,0,0)
 
-screen = pygame.display.set_mode(resolution)
+screen = pygame.display.set_mode(resolution,pygame.DOUBLEBUF|pygame.HWSURFACE)
 
 class Button:
     def __init__(self,color,key,xx,yy):
@@ -87,7 +87,7 @@ while True:
         for button in buttons:
             button.update(event)
 
-    screen.fill((0,0,0))
+    screen.fill((0,255,0))
     for button in buttons:
         button.draw()
     pygame.display.flip()
