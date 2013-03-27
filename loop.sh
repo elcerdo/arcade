81db24e311bcd663d732d50b00197b3e261fa009
Pierre Gueth <pierre.gueth@gmail.com>
2013-03-27 20:52:44 +0100

    quit

--- loop.sh
+++ loop.sh
@@ -9,7 +9,7 @@ logfilename="${HOME}/arcade_log"
 launcher_index=0
 while :
 do
-    launcher_log="$(${basedir}/launcher.py -f -i ${launcher_index})"
+    launcher_log="$(${basedir}/launcher.py -f -i ${launcher_index} || exit 1)"
     launcher_index="$(echo "${launcher_log}" | awk -F '=' '/INDEX/ {print $2}')"
     launcher_title="$(echo "${launcher_log}" | awk -F '=' '/TITLE/ {print $2}')"
     launcher_command="$(echo "${launcher_log}" | awk -F '=' '/COMMAND/ {print $2}')"
