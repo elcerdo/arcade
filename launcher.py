2009fb12ffde93ceaade537892ab3a22de97848e
Pierre Gueth <georges.wbushjr@laposte.net>
2013-03-28 14:23:45 +0100

    started help screen

--- launcher.py
+++ launcher.py
@@ -59,6 +59,87 @@ class Colors:
     background = black
     font = white
 
+# Elements
+class Button:
+    def __init__(self,color,xx,yy,phase,period=2.,up=.1):
+        self.period = period
+        self.up = up
+        self.phase = phase
+        self.state = False
+        self.up_sprite = pygame.image.load("%s_up.png" % color)
+        self.down_sprite = pygame.image.load("%s_down.png" % color)
+        self.up_rect = self.up_sprite.get_rect()
+        self.down_rect = self.down_sprite.get_rect()
+        self.move_to(xx,yy)
+    def move_to(self,xx,yy):
+        self.up_rect.left = xx
+        self.down_rect.left = xx
+        self.up_rect.top = yy
+        self.down_rect.bottom = self.up_rect.bottom
+    def update(self,current_time):
+        self.state = False
+        current_phase = (current_time/self.period - self.phase) % 1
+        if current_phase<self.up:
+            self.state = True
+    def draw(self):
+        if self.state:
+            screen.blit(self.down_sprite,self.down_rect)
+            return
+        screen.blit(self.up_sprite,self.up_rect)
+
+class Joystick:
+    def __init__(self,xx,yy,period=2.):
+        self.period = period
+        self.state_right = False
+        self.state_left = False
+        self.middle_sprite = pygame.image.load("joystick_middle.png")
+        self.right_sprite = pygame.image.load("joystick_right.png")
+        self.left_sprite = pygame.image.load("joystick_left.png")
+        self.middle_rect = self.middle_sprite.get_rect()
+        self.right_rect = self.right_sprite.get_rect()
+        self.left_rect = self.left_sprite.get_rect()
+        self.move_to(xx,yy)
+    def move_to(self,xx,yy):
+        self.middle_rect.left = xx
+        self.right_rect.left = xx
+        self.left_rect.right = self.middle_rect.right
+        self.middle_rect.top = yy
+        self.right_rect.bottom = self.middle_rect.bottom
+        self.left_rect.bottom = self.middle_rect.bottom
+    def update(self,current_time):
+        current_phase = (current_time/self.period) % 1
+        if current_phase<.25:
+            self.state_right = False
+            self.state_left  = True
+            return
+        if current_phase<.5:
+            self.state_right = False
+            self.state_left  = False
+            return
+        if current_phase<.75:
+            self.state_right = True
+            self.state_left  = False
+            return
+        self.state_right = False
+        self.state_left  = False
+    def draw(self):
+        if self.state_right and not self.state_left:
+            screen.blit(self.right_sprite,self.right_rect)
+            return
+        if self.state_left and not self.state_right:
+            screen.blit(self.left_sprite,self.left_rect)
+            return
+        screen.blit(self.middle_sprite,self.middle_rect)
+
+elements = [
+    Button("red",50,50,0),
+    Button("yellow",100,100,.2),
+    Button("green",150,50,.4),
+    Button("blue",200,100,.6),
+    Button("white",250,50,.8),
+    Joystick(100,150),
+    ]
+
 # Set screen
 resolution = (1024,768)
 screen_flags = pygame.DOUBLEBUF|pygame.HWSURFACE
@@ -147,8 +228,12 @@ while True:
             while len(events)>5:
                 events.pop(0)
 
+    current_time = time.time()
+    for element in elements:
+        element.update(current_time)
+
     if index_frame>0:
-        index_frame -=1
+        index_frame -= 1
     elif index_direction:
         index_frame = 5
         index_target += index_direction
@@ -167,7 +252,7 @@ while True:
         title = game[0]
         this_font_size = font_size-abs(delta_index_current)*font_size_progression
         this_left      = left+left_progression*delta_index_current*delta_index_current
-        this_baseline  = resolution[1]/2+(spacing+(font_size+this_font_size)/2)*delta_index_current
+        this_baseline  = 200+(spacing+(font_size+this_font_size)/2)*delta_index_current
         this_color     = Colors.white
         if delta_index==0:
             this_color = colors.get_at(((frame*10)%colors.get_width(),0))
@@ -180,6 +265,10 @@ while True:
         blit_text_multiline("INDEX\ntarget=%d\ncurrent=%.2f\nresidual=%.2f\ncurrent_round=%d\nframe=%d" % (index_target,index_current,index_residual,round(index_current),index_frame),20,10,10)
         blit_text_multiline("JOYSTICK\ncount=%d\nlast=%d\n\nframe=%d\nfps=%.1f" % (pygame.joystick.get_count(),joystick_last,frame,frame/(time_last-time_start)),20,140,10)
         blit_text_multiline("EVENT\n"+"\n".join(events),20,270,10)
+
+    for element in elements:
+        element.draw()
+
     pygame.display.flip()
 
     time.sleep(10e-3)
